f878b26b81980fb665f5386affdaecc6de603e4b
Anthony Scopatz <scopatz@gmail.com>
2017-11-09 21:01:01 -0500

    some fixes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,18 @@ setlocal EnableDelayedExpansion
 SET UNIX_PREFIX=%CYGWIN_PREFIX:~9%
 echo UNIX_PREFIX is %UNIX_PREFIX%
 
-bash configure --prefix %UNIX_PREFIX% --build=i386-pc-mingw32 --disable-static
+SET PKG_CONFIG_PATH=%UNIX_PREFIX%/lib/pkgconfig
+SET GLIBMM_CFLAGS=-L%UNIX_PREFIX%/Library/usr/bin -lmsys-glib-2.0-0 -lmsys-gobject-2.0-0 -lmsys-gmodule-2.0-0
+SET GLIBMM_LIBS=-L%UNIX_PREFIX%/Library/usr/bin -lmsys-glib-2.0-0 -lmsys-gobject-2.0-0 -lmsys-gmodule-2.0-0
+SET GIOMM_CFLAGS=-L%UNIX_PREFIX%/Library/usr/bin -lmsys-gio-2.0-0
+SET GIOMM_LIBS=-L%UNIX_PREFIX%/Library/usr/bin -lmsys-gio-2.0-0
+REM echo GLIBMM_LIBS are "%GLIBMM_LIBS%"
+SET LDFLAGS=-L%UNIX_PREFIX%/Library/usr/bin
+SET LIBS=-lmsys-glib-2.0-0 -lmsys-gobject-2.0-0 -lmsys-gmodule-2.0-0 -lmsys-gio-2.0-0
+SET PATH=%UNIX_PREFIX%/Library/usr/bin;%PATH%
+
+bash configure --prefix %UNIX_PREFIX% --build=i386-pc-mingw32 --disable-static --disable-documentation ^
+     --libdir %UNIX_PREFIX%/Library/usr/bin --disable-dependency-tracking 
 if errorlevel 1 exit 1
 
 make
