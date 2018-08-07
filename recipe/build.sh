d1067f87e1ee0603852f23dad20464552e3fccf8
Anthony Scopatz <scopatz@gmail.com>
2018-08-07 16:39:31 -0400

    modify and update recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
 export CFLAGS="-O3"
 export CXXFLAGS="-O3"
-if [ "$(uname)" == "Darwin" ]; then
-  # for Mac OSX
-  export CC=clang
-  export CXX=clang++
-  export MACOSX_VERSION_MIN="10.7"
-  export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
-  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
-  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export LDFLAGS="${LDFLAGS} -stdlib=libc++ -lc++"
-  export LINKFLAGS="${LDFLAGS}"
-fi
 
 # configure, make, install, check
 sed -e '/^libdocdir =/ s/$(book_name)/glibmm-'"${PKG_VERSION}"'/' \
